eb0da98203de3edf0915fa06be07620a305d762f
Alex Singh <alex@alexsingh.com>
2020-10-01 16:34:14 -0300

    update Glif Vault image
    Add InlineBox shared component
    
    Update page with new GlifCard component. Misc updates and code cleanup.
    
    Update metadata
    
    Replace LinkWrapper with StyledATag
    
    Center header text
    
    Delete unused component
    
    reduce font-size on cards
    
    Move GlifCard styles into the A element.
    
    Subtle letter spacing for GlifCard text
    
    Updating responsive type in GlifCards
    
    Shorten verify text
    
    Update package.json
    
    Upgrade next
    
    Fix security issue
    
    Fix security?

--- components/Home/SubtitleLogo.jsx
+++ components/Home/SubtitleLogo.jsx
@@ -6,43 +6,33 @@ import { MenuItem, Box, IconGlif, Title } from '../Shared'
 
 const TitleCopy = styled(Title)`
   /* Used this: https://stackoverflow.com/questions/14431411/pure-css-to-make-font-size-responsive-based-on-dynamic-amount-of-characters */
-  font-size: calc(48px + (64 - 40) * (100vw - 360px) / (1440 - 360));
+  font-size: 4rem;
 `
 
-const SubtitleLogo = ({ alt, color, text, imageUrl }) => {
+const SubtitleLogo = ({ alt, fill, imageUrl }) => {
   return (
-    <MenuItem display='flex' alignItems='center' width={['100%', 'auto']}>
       <Box
         display='flex'
         alignItems='center'
-        justifyContent='space-between'
+        justifyContent='center'
         py={4}
-        mr={[0, 6]}
         mb={[3, 0]}
         px={3}
         borderRadius={4}
-        width={['100%', 'auto']}
-        minWidth={11}
+        size={['120px', 9]}
 
         css={`
-          background: url(${imageUrl}) center no-repeat;
-          background-size: 100%;
+          background: url(${imageUrl}) center center no-repeat;
+          background-size: 160%;
           border-radius: 16px;
           alt: ${alt};
         `}
       >
-        <IconGlif
-          fill='#fff'
-          size={[6, 7, 8]}
-          css={`
-            transform: rotate(-90deg);
-          `}
-        />
-        <TitleCopy color={color} ml={3}>
-          {text}
-        </TitleCopy>
-      </Box>
-    </MenuItem>
+       
+        <IconGlif fill={fill} size={[5,6,7]} />
+      
+     
+        </Box>
   )
 }
 
@@ -55,7 +45,7 @@ SubtitleLogo.propTypes = {
 
 SubtitleLogo.defaultProps = {
   alt: '',
-  color: 'core.black'
+  fill: 'core.black'
 }
 
 export default SubtitleLogo
--- components/Home/index.jsx
+++ components/Home/index.jsx
@@ -2,10 +2,27 @@ import React, { useState } from 'react'
 import { string } from 'prop-types'
 import styled from 'styled-components'
 import axios from 'axios'
-import { space, layout, typography, border, color } from 'styled-system'
-import { Box, Menu, MenuItem, Title, Text, IconGlif, Subtitle } from '../Shared'
+import {
+  space,
+  layout,
+  typography,
+  border,
+  color,
+  boxShadow,
+  flexbox
+} from 'styled-system'
+import {
+  Box,
+  InlineBox,
+  Menu,
+  MenuItem,
+  Title,
+  Text,
+  IconGlif,
+  Subtitle
+} from '../Shared'
 import SubtitleLogo from './SubtitleLogo'
-import ExitLink, { LinkWrapper } from './ExitLink'
+import { StyledATag } from '../Shared/Link'
 
 const ButtonSignUp = styled.button`
   outline: none;
@@ -37,62 +54,63 @@ const InputEmail = styled.input`
   ${color}
 `
 
-// To be paired with MiniGlifIcon
-const MiniGlifCopy = styled(Title)`
-  font-size: ${(props) => props.theme.fontSizes[6]};
-  color: transparent;
-  -webkit-text-stroke: 1px #000;
+const GlifCardLinkWrapper = styled.a`
+  display: block;
+  text-decoration: none;
+  color: inherit;
+  transition: 0.24s ease-in-out;
+  ${space}
+  ${layout}
+  ${border} 
+  ${boxShadow}
+  ${flexbox}
+
+  &:hover {
+    transform: translate(8px, -8px);
+  }
 `
 
-// To be used as a quicklink shortcut in the future
-const MiniGlifIcon = ({ backgroundImg }) => {
+const GlifCard = ({ title, fill, description, imageUrl, href, linkName }) => {
   return (
-    <Box
-      borderRadius={3}
-      mx={2}
-      size={7}
-      css={`
-        background-position: center center;
-        background-size: cover;
-        background-image: url(${backgroundImg});
-      `}
-    ></Box>
-  )
-}
-
-// Example usage
-/* <MiniGlifCopy
-              fontSize={6}
-              css={`
-                -webkit-text-outline: 1px solid #444;
-              `}
-            >
-              VERIFY
-            </MiniGlifCopy> 
-  <Box
-            mr='8%'
-            width={['100%', 'auto']}
-            display='flex'
-            justifyContent='center'
+    <GlifCardLinkWrapper
+      href={href}
+      target='_blank'
+      display='flex'
+      alignItems='center'
+      justifyContent='flex-start'
+      flexWrap='wrap'
+      maxWidth={13}
+      p={3}
+      my={[4, 5]}
+      boxShadow={2}
+      borderRadius={4}
+    >
+      <SubtitleLogo
+        alt='Source: https://www.nontemporary.com/post/190437968500'
+        text='Wallet'
+        imageUrl={imageUrl}
+        fill={fill}
+      />
+      <Box>
+        <Title fontSize={6} mt={[0, 3]}>
+          {title}
+        </Title>
+        <Box display='flex'>
+          <Title
+            color='core.darkgray'
+            fontSize={[5, '2.5rem']}
+            my={4}
+            letterSpacing='0.015rem'
           >
-            <MiniGlifIcon backgroundImg='/imgtools.png' />
-            <MiniGlifIcon backgroundImg='/imgverify.png' />
-            <MiniGlifIcon backgroundImg='/imgnode.png' /> 
-  </Box>
-  */
-
-const Sentence = ({ sentenceStr }) => {
-  return sentenceStr.split(' ').map((word, i) => {
-    return (
-      <MenuItem key={i} mr='8%' my={[2, 3, 5]}>
-        <Subtitle>{word}</Subtitle>
-      </MenuItem>
-    )
-  })
-}
-
-Sentence.propTypes = {
-  sentenceStr: string.isRequired
+            {description}{' '}
+            <InlineBox fontSize={[5, 6]} color='#0051ff'>
+              {linkName}
+            </InlineBox>{' '}
+          </Title>
+        </Box>
+      </Box>
+    </GlifCardLinkWrapper>
+  )
 }
 
 export default () => {
@@ -142,235 +160,212 @@ export default () => {
           alignItems='center'
           justifyContent='center'
           flexWrap='wrap'
-          mb={8}
+          maxWidth={18}
+          margin='0 auto'
         >
-          <Sentence sentenceStr='Glif is an interoperable set of tools' />
-
-          <Box py={4} mr='8%' px={2} borderRadius={4} border={1} bg='black'>
-            <IconGlif size={[6, 7, 8]} p={2} position='relative' fill='white' />
+          <Box py={4} my={4} px={2} borderRadius={4} border={1} bg='black'>
+            <IconGlif size={[7, 8]} p={2} position='relative' fill='white' />
           </Box>
-          <Sentence sentenceStr='for the Filecoin network.' />
-          <MenuItem
-            display='flex'
-            flexWrap='wrap'
-            alignItems='baseline'
-            justifyContent={['center', 'space-between']}
-            width='100%'
-            color='core.darkgray'
-            my={[2, 3]}
-          >
-            {clicked ? (
-              <>
-                <Menu display='flex' alignItems='center'>
-                  <MenuItem>
-                    <Box
-                      display='flex'
-                      flexWrap='wrap'
-                      width={['100%', 'auto']}
-                      maxWidth={13}
-                    >
-                      <InputEmail
-                        width='100%'
-                        fontSize={[4, 5, 6]}
-                        color='core.nearblack'
-                        border={1}
-                        borderWidth={2}
-                        px={3}
-                        py={3}
-                        textAlign='center'
-                        placeholder='Your email, please'
-                        borderTopLeftRadius={4}
-                        borderTopRightRadius={4}
-                        onChange={(e) => setEmail(e.target.value)}
-                      />
-                      <ButtonSignUp
-                        width='100%'
-                        color='core.white'
-                        bg='core.nearblack'
-                        fontSize={[4, 5, 6]}
-                        border={1}
-                        borderColor='core.nearblack'
-                        borderWidth={2}
-                        borderBottomLeftRadius={4}
-                        borderBottomRightRadius={4}
-                        px={6}
-                        py={3}
-                        height='max-content'
-                        onClick={postToMailChimp}
-                      >
-                        Submit
-                      </ButtonSignUp>
-                    </Box>
-
-                    {error ? (
-                      <Title mt={2} color='red'>
-                        {error}
-                      </Title>
-                    ) : (
-                      <Title mt={2}>
-                        Glif don&rsquo;t spam! Unsub whenever.
-                      </Title>
-                    )}
-                    {subscribed && (
-                      <Title mt={2} color='status.success.background'>
-                        You&rsquo;re subscribed. Keep an eye out.
-                      </Title>
-                    )}
-                  </MenuItem>
-                </Menu>
-                <ButtonSignUp
-                  width={['100%', 'auto']}
-                  background='transparent'
-                  color='core.nearblack'
-                  fontSize={[4, 5, 6]}
-                  border={1}
-                  px={6}
-                  py={2}
-                  my={2}
-                  height='max-content'
-                  borderRadius={4}
-                  onClick={() => setClicked(false)}
-                >
-                  Cancel
-                </ButtonSignUp>
-              </>
-            ) : (
-              <>
-                <Text fontSize={[4, 5, 6]} my={2}>
-                  Be the first to learn when we launch
-                </Text>
-
-                <ButtonSignUp
-                  background='transparent'
-                  color='core.nearblack'
-                  fontSize={[4, 5, 6]}
-                  border={1}
-                  px={6}
-                  py={2}
-                  height='max-content'
-                  borderRadius={4}
-                  onClick={() => setClicked(true)}
-                >
-                  Sign Up
-                </ButtonSignUp>
-              </>
-            )}
-          </MenuItem>
+          <Title textAlign='center' fontSize={[5, 6]}>
+            Glif is an interoperable set of tools for the Filecoin network
+          </Title>
         </Menu>
       </section>
-      <section name='Glif Wallet'>
-        <Menu
-          display='flex'
-          alignItems='center'
-          justifyContent='flex-start'
-          flexWrap='wrap'
-          mb={9}
-        >
-          <SubtitleLogo
-            alt='Source: https://www.nontemporary.com/post/190437968500'
-            text='Wallet'
+
+      <Box
+        display='flex'
+        flexWrap='wrap'
+        justifyContent='space-around'
+        mt={[4, 6]}
+      >
+        <section name='Glif Wallet'>
+          <GlifCard
             imageUrl='/imgtools.png'
+            title='Wallet'
+            description='A lightweight interface for sending and receiving Filecoin.'
+            href='https://wallet.glif.io'
+            linkName='Go↗'
           />
-          <Sentence sentenceStr='A lightweight interface for sending and receiving Filecoin.' />
-          <ExitLink linkName='Go' href='https://wallet.glif.io' />
-        </Menu>
-      </section>
-      <section name='Glif Vault'>
-        <Menu
-          display='flex'
-          alignItems='center'
-          justifyContent='flex-start'
-          flexWrap='wrap'
-          mb={9}
-        >
-          <SubtitleLogo
-            alt='Source: https://www.nontemporary.com/post/190437968500'
-            text='Vault'
-            imageUrl='/imgsaft.png'
-            color='white'
+        </section>
+
+        <section name='Glif Vault'>
+          <GlifCard
+            imageUrl='/imgvault.png'
+            fill='#fff'
+            title='Vault'
+            description='Use your Ledger device to hold your Filecoin SAFT.
+'
+            href='https://wallet.glif.io/vault'
+            linkName='Go↗'
           />
-          <Sentence sentenceStr='Use your Ledger device to hold your Filecoin SAFT.' />
-          <ExitLink linkName='Go' href='https://wallet.glif.io/vault' />
-        </Menu>
-      </section>
-      <section name='Glif Faucet'>
-        <Menu
-          display='flex'
-          alignItems='center'
-          justifyContent='flex-start'
-          flexWrap='wrap'
-          mb={9}
-        >
-          <SubtitleLogo
-            alt='Source: https://unsplash.com/photos/g2Zf3hJyYAc'
-            text='Faucet'
+        </section>
+
+        <section name='Glif Faucet'>
+          <GlifCard
             imageUrl='/imgfaucet.jpg'
+            fill='#000'
+            title='Faucet'
+            description='Quickily, easily receive testnet Filecoin.'
+            href='https://faucet.glif.io'
+            linkName='Go↗'
           />
-          <Sentence sentenceStr='Quickily, easily receive testnet Filecoin.' />
-          <ExitLink linkName='Go' href='https://faucet.glif.io' />
-        </Menu>
-      </section>
-      <section name='Glif Verify'>
-        <Menu
-          display='flex'
-          alignItems='center'
-          justifyContent='flex-start'
-          flexWrap='wrap'
-          mb={9}
-        >
-          <SubtitleLogo color='white' text='Verify' imageUrl='/imgverify.png' />
-          <Sentence sentenceStr='Earn "Verified" Filecoin storage when you verify yourself.' />
-          <ExitLink linkName='Go' href='https://verify.glif.io' />
-        </Menu>
-      </section>
-      <section name='Glif Node'>
-        <Menu
-          display='flex'
-          alignItems='center'
-          justifyContent='flex-start'
-          flexWrap='wrap'
-          mb={9}
-        >
-          <SubtitleLogo
-            alt='Credit & Source: https://www.nontemporary.com/post/187451107349/rob-nick-carter'
+        </section>
+
+        <section name='Glif Verify'>
+          <GlifCard
+            imageUrl='/imgverify.png'
+            fill='#fff'
+            title='Verify'
+            description='Receive verified Filecoin storage.'
+            href='https://verify.glif.io'
+            linkName='Go↗'
+          />
+        </section>
+
+        <section name='Glif Node'>
+          <GlifCard
             imageUrl='/imgnode.png'
-            text='Nodes'
+            fill='#fff'
+            title='Nodes'
+            description='Public and dedicated Filecoin node infrastructure.'
+            href='mailto:squad@infinitescroll.org'
+            linkName='Email Us↗'
           />
+        </section>
+      </Box>
 
-          <Sentence sentenceStr='Public and dedicated Filecoin node infrastructure.' />
-          <ExitLink linkName='Email Us' href='mailto:ahoy@openworklabs.com' />
-        </Menu>
-      </section>
-      <section name='Made by OWL'>
-        <Menu>
-          <MenuItem display='flex' alignItems='center' my={[2, 3, 5]}>
-            <IconGlif size={6} />
+      <section name='Sign uuuup'>
+        <MenuItem
+          display='flex'
+          flexWrap='wrap'
+          alignItems='baseline'
+          justifyContent={['center', 'space-between']}
+          width='100%'
+          color='core.darkgray'
+          my={[2, 4, 8]}
+        >
+          {clicked ? (
+            <>
+              <Menu display='flex' alignItems='center'>
+                <MenuItem>
+                  <Box
+                    display='flex'
+                    flexWrap='wrap'
+                    width={['100%', 'auto']}
+                    maxWidth={13}
+                  >
+                    <InputEmail
+                      width='100%'
+                      fontSize={[4, 5, 6]}
+                      color='core.nearblack'
+                      border={1}
+                      borderWidth={2}
+                      px={3}
+                      py={3}
+                      textAlign='center'
+                      placeholder='Your email, please'
+                      borderTopLeftRadius={4}
+                      borderTopRightRadius={4}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <ButtonSignUp
+                      width='100%'
+                      color='core.white'
+                      bg='core.nearblack'
+                      fontSize={[4, 5, 6]}
+                      border={1}
+                      borderColor='core.nearblack'
+                      borderWidth={2}
+                      borderBottomLeftRadius={4}
+                      borderBottomRightRadius={4}
+                      px={6}
+                      py={3}
+                      height='max-content'
+                      onClick={postToMailChimp}
+                    >
+                      Submit
+                    </ButtonSignUp>
+                  </Box>
 
-            <Title my={0} mx={2} display='flex'>
-              is an
-              <LinkWrapper
-                href='https://www.openworklabs.com'
-                borderBottom={1}
-                borderWidth={3}
-                fontSize={4}
-                mx={1}
-                target='_blank'
-                css={`
-                  &:hover {
-                    color: #0051ff;
-                    background: transparent;
-                  }
-                  ::before {
-                    border: 0;
-                  }
-                `}
+                  {error ? (
+                    <Title mt={2} color='red'>
+                      {error}
+                    </Title>
+                  ) : (
+                    <Title mt={2}>Glif don&rsquo;t spam! Unsub whenever.</Title>
+                  )}
+                  {subscribed && (
+                    <Title mt={2} color='status.success.background'>
+                      You&rsquo;re subscribed. Keep an eye out.
+                    </Title>
+                  )}
+                </MenuItem>
+              </Menu>
+              <ButtonSignUp
+                width={['100%', 'auto']}
+                background='transparent'
+                color='core.nearblack'
+                fontSize={[4, 5, 6]}
+                border={1}
+                px={6}
+                py={2}
+                my={2}
+                height='max-content'
+                borderRadius={4}
+                onClick={() => setClicked(false)}
               >
-                OWL
-              </LinkWrapper>
-              project {'\u00A9'} 2020
-            </Title>
-          </MenuItem>
-        </Menu>
+                Cancel
+              </ButtonSignUp>
+            </>
+          ) : (
+            <>
+              <Text fontSize={[4, 5, 6]} textAlign='center' my={2}>
+                Be the first to learn when we launch new glifs
+              </Text>
+
+              <ButtonSignUp
+                background='transparent'
+                color='core.nearblack'
+                fontSize={[4, 5, 6]}
+                border={1}
+                px={6}
+                py={2}
+                height='max-content'
+                borderRadius={4}
+                onClick={() => setClicked(true)}
+              >
+                Sign Up
+              </ButtonSignUp>
+            </>
+          )}
+        </MenuItem>
+      </section>
+      <section name='Made by IS'>
+        <Box
+          display='flex'
+          alignItems='center'
+          flexGrow='1'
+          width='100%'
+          mt={6}
+          mb={3}
+        >
+          <IconGlif size={6} />
+          <Title fontSize={4} my={0} mx={2}>
+            is part of the
+            <StyledATag
+              display='inline-block'
+              href='https://www.infinitescroll.org'
+              borderBottom={1}
+              borderWidth={3}
+              fontSize={4}
+              mx={1}
+              target='_blank'
+            >
+              Infinite Scroll
+            </StyledATag>
+            {'\u00A9'} 2020
+          </Title>
+        </Box>
       </section>
     </Box>
   )
