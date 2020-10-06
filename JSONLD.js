f585e9ca65227b75c9731dca60bb5d7d63a76445
listenaddress <tbobrien612@gmail.com>
2020-10-06 00:24:29 -0400

    Update JSONLD

--- JSONLD.js
+++ JSONLD.js
@@ -1,11 +1,48 @@
 export default {
-  '@context': 'http://schema.org/',
   '@type': 'WebApplication',
-  name: 'Glif Verifier',
-  description: 'Granting verified data allowances to Filecoin addresses.',
-  url: 'https://verify.glif.io',
-  applicationCategory: 'Blockchain wallet',
-  operatingSystem: 'All',
+  name: 'Glif',
+  description:
+    'A set of tools for the Filecoin network, made by Infinite Scroll.',
+  url: 'https://glif.io',
+  sameAs: ['https://github.com/glifio'],
+  owns: [
+    {
+      '@type': 'WebApplication',
+      name: 'Glif Wallet',
+      description:
+        'A web wallet to manage your Filecoin on your Ledger device.',
+      applicationCategory: 'Cryptocurrency wallet',
+      operatingSystem: 'All'
+    },
+    {
+      '@type': 'WebApplication',
+      name: 'Glif Nodes',
+      description: 'Private and public gateways to the Filecoin network.',
+      applicationCategory: 'Blockchain node infrastructure',
+      operatingSystem: 'All'
+    },
+    {
+      '@type': 'WebApplication',
+      name: 'Glif Vault',
+      description: 'Use your Ledger device to hold your Filecoin SAFT.',
+      applicationCategory: 'Cryptocurrency wallet',
+      operatingSystem: 'All'
+    },
+    {
+      '@type': 'WebApplication',
+      name: 'Glif Faucet',
+      description: 'Quickly, easily receive Filecoin to test and develop with.',
+      applicationCategory: 'Cryptocurrency faucet',
+      operatingSystem: 'All'
+    },
+    {
+      '@type': 'WebApplication',
+      name: 'Glif Verifier',
+      description: 'Earn "verified" Filecoin storage when you verify yourself.',
+      applicationCategory: 'Blockchain user validation',
+      operatingSystem: 'All'
+    }
+  ],
   knowsAbout: [
     {
       '@type': 'SoftwareApplication',
@@ -15,24 +52,20 @@ export default {
       operatingSystem: 'All'
     },
     {
-      '@type': 'WebApplication',
-      name: 'Glif Wallet',
-      description:
-        'A web wallet to manage your Filecoin on your Ledger device.',
-      applicationCategory: 'Blockchain wallet',
-      operatingSystem: 'All'
-    },
+      '@type': 'Corporation',
+      name: 'Ledger SAS',
+      url: 'https://www.ledger.com/'
+    }
   ],
   parentOrganization: {
     '@type': 'Organization',
-    name: 'Open Work Labs',
-    description:
-      'A product studio building tools for open work and the distributed web.',
-    url: 'https://openworklabs.com',
+    name: 'Infinite Scroll',
+    description: 'Self-sustaining systems for the worlds to come.',
+    url: 'https://infinitescroll.org',
     sameAs: [
-      'https://github.com/openworklabs',
-      'https://twitter.com/openworklabs',
-      'https://www.are.na/open-work-labs'
+      'https://github.com/infinitescroll',
+      'https://twitter.com/infinitescroll_',
+      'https://www.are.na/infinite-scroll'
     ]
   }
 }
