967ea98b85ef641a3d3399c43ff5656ee20e569c
Thijs van Hoof <thijs.v.h@gmail.com>
2022-03-04 20:58:29 +0100

    Page layout (#51)
    * Use new layout on homepage
    
    * Bump base-css version
    
    * Fix lint err
    
    * Bump react-components version
    
    * Adjust homepage AppHeaderLinks
    
    * Bump react-components version
    
    * Update snapshots
    
    Co-authored-by: Jon Schwartz <jpschwartz2@uwalumni.com>

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,85 +1,32 @@
 import React from 'react'
 import {
-  AppHeader,
   AppTile,
   AppTilesWrapper,
-  Box,
-  Footer,
-  P,
-  fontSize,
-  space,
-  devices,
-  maxWidth,
-  theme
+  OneColumnLargeText,
+  Page,
+  Spacer,
+  SmartLink
 } from '@glif/react-components'
-import styled from 'styled-components'
-
-const TextBox = styled.div`
-  font-size: ${fontSize('large')};
-  border-radius: 8px;
-  margin-top: ${space()};
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-
-  @media (max-width: ${devices.phone}) {
-    padding: ${space('large', 'phone')};
-  }
-
-  @media (min-width: ${devices.phone}) and (max-width: ${devices.tablet}) {
-    padding: 40px ${space('large', 'tablet')};
-  }
-
-  @media (min-width: ${devices.tablet}) {
-    padding: 80px ${space('large', 'desktop')};
-  }
-`
-
-const StyledText = styled(P)`
-  font-size: inherit;
-  max-width: 670px;
-
-  > a {
-    font-size: inherit;
-
-    &:hover {
-      text-decoration: none;
-      color: inherit;
-    }
-  }
-`
-
-const BoxStyled = styled(Box)`
-  @media (max-width: ${devices.phone}) {
-    padding: ${space('default', 'phone')};
-  }
-
-  @media (min-width: ${devices.phone}) {
-    padding: ${space()};
-  }
-`
-
-const AppTilesWrapperStyled = styled(AppTilesWrapper)`
-  max-width: ${maxWidth};
-  margin-left: auto;
-  margin-right: auto;
-
-  @media (max-width: ${devices.tablet}) {
-    width: calc(100% + (${space('default', 'phone')} * 2));
-    margin-left: calc(${space('default', 'phone')}*-1);
-  }
-`
 
 export default function AppsHome() {
   return (
-    <BoxStyled>
-      <AppHeader
-        homeHref=''
-        blogHref='https://blog.glif.io'
-        codeHref='https://github.com/glifio/'
-        nodesHref='https://lotus.filecoin.io/docs/developers/hosted-lotus/'
-      />
-      <AppTilesWrapperStyled>
+    <Page
+      appHeaderLinks={[
+        {
+          title: 'Blog',
+          url: 'https://glif.io/blog'
+        },
+        {
+          title: 'Code',
+          url: 'https://github.com/glifio'
+        },
+        {
+          title: 'Nodes',
+          url: 'https://lotus.filecoin.io/docs/developers/hosted-lotus'
+        }
+      ]}
+    >
+      <AppTilesWrapper>
         <AppTile
           title='Wallet'
           description='A lightweight interface for sending and receiving Filecoin.'
@@ -114,43 +61,39 @@ export default function AppsHome() {
           imgSrcHover='/bg-verifier-hover.jpg'
           small
         />
-      </AppTilesWrapperStyled>
-      <TextBox>
-        <StyledText>
+      </AppTilesWrapper>
+
+      <Spacer />
+
+      <OneColumnLargeText>
+        <p>
           Instead of the fortress, there are numerous fortresses now, and
           various people have keys.
-        </StyledText>
-        <StyledText>
+        </p>
+        <p>
           The treasures are no longer stored in the secluded cellars and tombs
           of the few, but guarded by the many. A new language has evolved, used
           to keep the treasures safe and allow those who watch over them to
           communicate, directly and discretely. The hieroglyphic system consists
           only of geometric shapes, as an homage to the interconnected data
           centers with their cages, racks and cabinets.
-        </StyledText>
-        <StyledText>
+        </p>
+        <p>
           All miners shall have their own, personalised Glif, as a gesture of
           allegiance. (soon)
-        </StyledText>
-      </TextBox>
+        </p>
+      </OneColumnLargeText>
 
-      <TextBox
-        style={{
-          color: 'white',
-          background: theme.colors.core.primary
-        }}
-      >
-        <StyledText>
+      <Spacer />
+
+      <OneColumnLargeText className='primary'>
+        <p>
           Glif offers a suite of interoperable tools for the{' '}
-          <a href='https://filecoin.io/' target='_blank' rel='noreferrer'>
-            Filecoin
-          </a>{' '}
-          network. With these tools, people can store, permission, and trade
-          their data in healthy and equitable ways.
-        </StyledText>
-      </TextBox>
-
-      <Footer />
-    </BoxStyled>
+          <SmartLink href='https://filecoin.io/'>Filecoin</SmartLink> network.
+          With these tools, people can store, permission, and trade their data
+          in healthy and equitable ways.
+        </p>
+      </OneColumnLargeText>
+    </Page>
   )
 }
