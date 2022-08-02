f70f1adb7a3a0543f9eb7758435752febc633914
Thijs van Hoof <thijs.v.h@gmail.com>
2022-08-02 22:18:56 +0200

    Update dependencies (#84)
    * Update dependencies
    
    * update tests
    
    * update dependencies
    
    * update react-components

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,29 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  collectCoverageFrom: ['**/*.{js,jsx}', '!**/node_modules/**'],
+  collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/node_modules/**'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   transform: {
-    '^.+\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest'
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
   },
   transformIgnorePatterns: [
     '/node_modules/',
     '^.+\\.module\\.(css|sass|scss)$'
   ],
   moduleNameMapper: {
-    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy'
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    'multiformats/basics':
+      '<rootDir>/node_modules/multiformats/cjs/src/basics.js',
+    'multiformats/cid': '<rootDir>/node_modules/multiformats/cjs/src/cid.js',
+    multiformats: '<rootDir>/node_modules/multiformats/cjs/src/index.js',
+    '@ipld/dag-cbor': '<rootDir>/node_modules/@ipld/dag-cbor/cjs/index.js',
+    cborg: '<rootDir>/node_modules/cborg/cjs/cborg.js',
+    uint8arrays: '<rootDir>/node_modules/uint8arrays/cjs/src/index.js'
   },
+  setupFilesAfterEnv: ['./jest.setup.js'],
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['./jest.setup.js']
+  modulePathIgnorePatterns: ['<rootDir>/.*/__mocks__'],
+  clearMocks: true,
+  fakeTimers: {
+    enableGlobally: true
+  }
 }
