e99d25673648c2463bc81834cdf414c027c77d9e
Thijs van Hoof <thijs.v.h@gmail.com>
2022-10-06 10:41:40 +0200

    Remove ThemeProvider (#109)
    * Remove ThemeProvider
    
    * Update index.spec.jsx.snap

--- components/index.spec.jsx
+++ components/index.spec.jsx
@@ -1,15 +1,10 @@
 import { render, screen } from '@testing-library/react'
-import { theme, ThemeProvider } from '@glif/react-components'
 
 import Home from './Home'
 
 describe('Apps home page', () => {
   test('it renders correctly', () => {
-    const { container } = render(
-      <ThemeProvider theme={theme}>
-        <Home />
-      </ThemeProvider>
-    )
+    const { container } = render(<Home />)
 
     expect(screen.getAllByText('Wallet')[0]).toBeInTheDocument()
     expect(screen.getAllByText('Safe')[0]).toBeInTheDocument()
